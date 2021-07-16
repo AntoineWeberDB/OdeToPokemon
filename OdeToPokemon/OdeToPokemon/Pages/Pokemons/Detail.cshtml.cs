95f553432bfe5a4e0786412487d8db00ed6e0e4b
WEBER Antoine <antoine.weber@soprasteria.com>
2021-07-16 11:47:43 +0200

    Added path from a pokemon detail to another

--- OdeToPokemon/OdeToPokemon/Pages/Pokemons/Detail.cshtml.cs
+++ OdeToPokemon/OdeToPokemon/Pages/Pokemons/Detail.cshtml.cs
@@ -12,23 +12,10 @@ namespace OdeToPokemon.Pages.Pokemons
     public class DetailModel : PageModel
     {
         public Pokemon Pokemon { get; set; }
-        public string PreviousEvolution { get; set; }
-        public string NextEvolution { get; set; }
 
         public void OnGet(int PokemonId,InMemoryPokemonData pokemonData)
         {
             Pokemon = pokemonData.GetPokemonById(PokemonId);
-            
-            PreviousEvolution = "-" ;
-            if (Pokemon.PreviousEvolution != null)
-            {
-                PreviousEvolution = Pokemon.PreviousEvolution.Name;
-            }
-            NextEvolution = "-" ;
-            if (Pokemon.NextEvolution != null)
-            {
-                NextEvolution = Pokemon.NextEvolution.Name;
-            } 
         }
     }
 }
