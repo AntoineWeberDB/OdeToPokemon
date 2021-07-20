9380fb0fc4101f2f78a01455a752497b7a6257c4
WEBER Antoine <antoine.weber@soprasteria.com>
2021-07-20 17:27:29 +0200

    Made Ajax Api worked

--- OdeToPokemon/OdeToPokemon/Pages/Pokemons/ClientPokemons.cshtml.cs
+++ OdeToPokemon/OdeToPokemon/Pages/Pokemons/ClientPokemons.cshtml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToPokemon.Core;
 
 namespace OdeToPokemon.Pages.Pokemons
 {
     public class ClientPokemonsModel : PageModel
     {
+   
         public void OnGet()
         {
 
